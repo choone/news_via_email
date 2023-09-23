766b502f9b0298a05e5b3d2f648512d6781dc8af
Name <user@example.com>
2023-09-23 16:05:29 +0200

    Send an email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import requests
+import requests, sendemail
 
 api_key = "0d817cf9e8d04c40b77782c0f27dc607"
-url = "https://newsapi.org/v2/everything?q=tesla\
-&from=2023-08-22&sortBy=publishedAt&\
-apiKey=0d817cf9e8d04c40b77782c0f27dc607"
+url = 'https://newsapi.org/v2/everything?q=tesla&from=2023-08-23\
+&sortBy=publishedAt&apiKey=0d817cf9e8d04c40b77782c0f27dc607'
 
 # Make request
 request = requests.get(url)
@@ -12,6 +11,10 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
+body = ""
 for article in content['articles']:
-    print(article["title"])
-    print(article["description"])
+    body = (body + article['title'] + "\n"
+            + article['description'] + "\n\n")
+
+body = body.encode("utf-8")
+sendemail.send_email(body)
---
+++ sendemail.py
@@ -0,0 +1,13 @@
+import smtplib, ssl, os
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "jakuborafal@gmail.com"
+    password = os.getenv("userpass")
+    context = ssl.create_default_context()
+    receiver = "jakuborafal@gmail.com"
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
