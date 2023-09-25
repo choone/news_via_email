376db0395575dda3b3e32fb6c7852d13218d147d
Name <user@example.com>
2023-09-25 17:34:13 +0200

    Add links, specify language, limit news, add subject, control category

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import requests, sendemail
 
 api_key = "0d817cf9e8d04c40b77782c0f27dc607"
-url = 'https://newsapi.org/v2/everything?q=tesla&from=2023-08-23\
-&sortBy=publishedAt&apiKey=0d817cf9e8d04c40b77782c0f27dc607'
+topic = "amazon"
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}"
+       "&from=2023-08-25"
+       "&sortBy=publishedAt"
+       "&apiKey=0d817cf9e8d04c40b77782c0f27dc607"
+       "&language=en")
 
 # Make request
 request = requests.get(url)
@@ -11,10 +16,11 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
-body = ""
-for article in content['articles']:
+body = "Subject: Today's news" + "\n"
+for article in content['articles'][:20]:
     body = (body + article['title'] + "\n"
-            + article['description'] + "\n\n")
+            + article['description'] + "\n"
+            + article['url'] + "\n\n")
 
 body = body.encode("utf-8")
 sendemail.send_email(body)
